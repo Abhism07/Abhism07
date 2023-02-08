ef7a4bd3ee9ad52ace0bba20bf3ff928eafc0c32
Abhishek Mandave <99401298+Abhism07@users.noreply.github.com>
2023-02-08 07:23:15 +0530

    Create bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "bits/stdc++.h"
+using namespace std;
+
+const int N= 9999;
+bool vis[N];
+vector <int> adj[N];
+
+int main()
+{
+	for(int i=0 ;i<N;i++)
+		vis[i]=0;
+	int n,m;
+	cout<<"Enter the number of nodes and edges:"<<endl;
+	cin>>n>>m;
+	int x,y;
+	for(int i=0 ;i<m;i++)
+	{
+	      cin>>x>>y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+	queue <int> q;
+	q.push(1);
+	vis[1] = true;
+
+	while(!q.empty())
+	{
+		int node = q.front();
+		q.pop();
+		cout<<node<<endl;
+		vector <int> :: iterator it;
+		for(it = adj[node].begin() ;it != adj[node].end();it++)
+		{
+			if(!vis[*it])
+			{
+				vis[*it]=1;
+				q.push(*it);
+			}
+		}
+	}
+
+}
