b5ec6c0671bca876a72cd23b47dc392070323a9e
Abhishek Mandave <99401298+Abhism07@users.noreply.github.com>
2023-05-30 09:17:52 +0530

    Update TransformExample.cs

--- TransformExample.cs
+++ TransformExample.cs
@@ -12,6 +12,7 @@ public class TransformExample : MonoBehaviour
         // Perform transformations
         TranslateObject();
         RotateObject();
+        ScaleObject();
     }
 
     private void TranslateObject()
@@ -24,7 +25,14 @@ public class TransformExample : MonoBehaviour
     private void RotateObject()
     {
         // Rotate the game object by a specific angle
-        Vector3 rotation = new Vector3(45f, 90f, 0f);
+        Vector3 rotation = new Vector3(0f, 90f, 0f);
         myTransform.rotation *= Quaternion.Euler(rotation);
+    }
+	 
+	 private void ScaleObject()
+    {
+        // Scale the game object by a specific factor
+        Vector3 scale = new Vector3(2f, 2f, 2f);
+        myTransform.localScale = Vector3.Scale(myTransform.localScale, scale);
     }
 }
